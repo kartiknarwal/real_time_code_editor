8386c074d1c6ecdfd89cef5d7be7100014d9066a
kartiknarwal <124709774+kartiknarwal@users.noreply.github.com>
2024-08-12 23:38:54 +0530

    added backend url of render on 5 th line

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css'
 import io from "socket.io-client"
 import Editor from "@monaco-editor/react";
-const socket =io("http://localhost:5000");
+const socket =io("https://real-time-code-editor-1-u4lu.onrender.com");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
